81ac8bc745a489f11bf534f6acb54fcb48d4714d
medbelka <margopv@gmail.com>
2022-11-24 23:00:38 +0200

    step2 withount count percentage

--- src/components/FeedbackCounter/Counter.jsx
+++ src/components/FeedbackCounter/Counter.jsx
@@ -10,19 +10,23 @@ class Counter extends Component {
         good: 0,
         neutral: 0,
         bad: 0,
+        totalFeedback: 0,
+        feedbackPositivePercentage: 0,
       }
     
     handleGoodIncrement = () => { this.setState(prevState =>({good: prevState.good + 1}))};
     handleNeutralIncrement = () => { this.setState(prevState =>({neutral: prevState.neutral + 1}))};
     handleBadIncrement = () => { this.setState(prevState =>({bad: prevState.bad + 1}))};
-    // countTotalFeedback = () => { }
-
+    countTotalFeedback = () => { this.setState(prevState =>({totalFeedback: prevState.totalFeedback + 1})) };
+    // function countPositiveFeedbackPercentage(good, totalFeedback) { const per = ((this.state.good)/this.state.totalFeedback)*100
+    //  return per
+    // };
 
       render() {
         return (
             <section className="Feedback">
             <h1>Please leave feedback</h1>
-            <div className="FeedbackOptions">
+            <div className="FeedbackOptions" onClick={this.countTotalFeedback}>
                 <button className="Button" type="button" onClick={this.handleGoodIncrement}>
                     Good
                 </button>
@@ -38,6 +42,8 @@ class Counter extends Component {
                 <span className="StatisticsTotal">Good : {this.state.good}</span>
                 <span className="StatisticsTotal">Neutral : {this.state.neutral}</span>
                 <span className="StatisticsTotal">Bad : {this.state.bad}</span>
+                <span className="StatisticsTotal">Total: {this.state.totalFeedback}</span>
+                {/* <span className="StatisticsTotal">Positive feedback: {this.state.feedbackPositivePercentage}%</span> */}
             </div>
             </section>
         )
